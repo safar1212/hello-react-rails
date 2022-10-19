00a7f4d3549ddc0a5c6127f34a1416102bddceb5
safar ali <miqra019@gmail.com>
2022-10-19 23:23:20 +0500

    Complete project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,30 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
 import React from "react";
 import ReactDOM from "react-dom";
+import { NavLink } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+import Greeting from "./controllers/components/Greeting";
 
-function App() {
-  return <h1>Hello World! This is a test</h1>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path="/greet" element={<Greeting />} />
+    </Routes>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <NavLink to="/" role="button">
+        <span>Home | </span>
+      </NavLink>
+      <NavLink to="/greet" role="button">
+        <span>Greet someone</span>
+      </NavLink>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'greetings#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "root#index"
+  
 end
